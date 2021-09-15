8fdce6663b36855a5f2ecba678aabe3bc0df438b
sidorova0303 <sidorovanp21@oiate.ru>
2021-09-15 10:47:05 +0300

    2

---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InformationBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(Environment.UserDomainName);
+            Console.WriteLine(Environment.UserName);
+            Console.WriteLine(Environment.Version);
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            //Устаревший Ванрайт
+            //Console.WriteLine(Environment.ProcessId);
+            Console.WriteLine(Environment.ProcessorCount);
+            Console.WriteLine(Environment.Is64BitProcess);
+            Console.WriteLine(Environment.Is64BitOperatingSystem);
+            //
+            Console.WriteLine();
+            Console.WriteLine(Console.ReadLine());
+
+        }
+    }
+}
